b44c7af6e5acbc1e12b61f2ef4d8487bea48a08e
Mirian Santos <mirian.trsantos@gmail.com>
2022-12-18 13:00:09 -0300

    nova funcao lambda para deletar um item

---
+++ src/deleteItem.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const AWS = require("aws-sdk")
+
+const deleteItem = async (event) => {
+
+  const {id} = event.pathParameters
+
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  await dynamodb.delete({
+    TableName: "ItemTable",
+    Key: {id}
+  
+  }).promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      { msg: 'Item Deleted'}
+    ),
+  };
+};
+
+
+module.exports = {
+    handler:deleteItem
+}
+
+
